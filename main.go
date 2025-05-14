97be8853baaa3bc7352a23a2d4c6371bdcaa1ee4
nolions <pkh_nolions@photons.com.tw>
2025-05-14 11:56:57 +0800

    [fix] 修正 使用範例

--- main.go
+++ main.go
@@ -1,25 +1,28 @@
 package main
 
 import (
-	"cricketNewsCrawler/crawler"
 	"fmt"
+	"github.com/Nolions/CricketNewsCrawler/crawler"
 	"log"
 )
 
 func main() {
-	cricbuzz := crawler.NewCricbuzz()
-	
+	cricbuzz := crawler.NewSportSkeeda()
+
 	newsList, err := cricbuzz.FetchNewsList()
 	if err != nil {
 		log.Fatalf("爬取新聞失敗: %v", err)
 	}
 
-	// 顯示結果
+	//顯示結果
 	for i, n := range newsList {
 		fmt.Printf("新聞 %d: %s\nLink: %s\nDesc: %s\nCover: %s\n\n", i+1, n.Title, n.Link, n.Desc, n.Cover)
 	}
 
-	content, _ := cricbuzz.FetchNewsDetail("https://www.cricbuzz.com/cricket-news/133952/shreyas-iyer-30-from-spin-hitter-to-modern-t20-marauder")
+	//news := crawler.News{}
 
-	fmt.Println("內文：", content)
+	//err := cricbuzz.FetchNewsDetail(" https://www.cricbuzz.com/cricket-news/134344/ipl-2025-to-resume-on-may-17-final-on-june-3", &news)
+	//
+	//fmt.Println(err)
+	//fmt.Println(news)
 }
